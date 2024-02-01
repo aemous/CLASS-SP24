3a44f9d15d115997f3e9714bfef9b7c9fe99f94e
aemous <amoustafa0518@gmail.com>
2024-02-01 12:37:03 -0500

    Update block size to 36.

--- hw1/dgemm-blocked.c
+++ hw1/dgemm-blocked.c
@@ -1,10 +1,15 @@
 const char* dgemm_desc = "Simple blocked dgemm.";
 
 #ifndef BLOCK_SIZE
-#define BLOCK_SIZE 63
+#define BLOCK_SIZE 36
 #endif
 
 #define min(a, b) (((a) < (b)) ? (a) : (b))
+
+/*
+ * So, it looks like this implementation basically does the naive dgemm, except on smaller
+ * blocks instead of the whole matrix.
+ */
 
 /*
  * This auxiliary subroutine performs a smaller dgemm operation
