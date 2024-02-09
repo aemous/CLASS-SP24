1748c0542052e0279482ffb2cb43748aab7b574c
aemous <amoustafa0518@gmail.com>
2024-02-09 08:21:20 -0500

    .

--- hw1/dgemm-blocked.c
+++ hw1/dgemm-blocked.c
@@ -1,7 +1,7 @@
 const char* dgemm_desc = "Simple blocked dgemm.";
 
 #include <immintrin.h>
-//#include <stdio.h>
+#include <stdio.h>
 
 // Define micro-block (kernel) size
 #ifndef BI_SIZE
@@ -37,8 +37,8 @@ static void do_block(int lda, int M, int N, int K, double* A, double* B, double*
 
 /*
  * This auxiliary subroutine performs a smaller dgemm operation
- *  C := C + A * B
- * where C is M-by-N, A and A_block are M-by-K, and B and B_block are K-by-N.
+ *  C_block := C_block + A_block * B_block
+ *  where A, B, and C are lda-by-lda, and A_block is M-by-K, B_block is K-by-N, and C_Block is M-by-N
  */
 static void do_micro_block(int lda,
                            int M,
@@ -51,15 +51,16 @@ static void do_micro_block(int lda,
                            double* B,
                            double* C,
                            double* A_block,
-                           double* B_block) {
+                           double* B_block,
+                           double* C_block) {
     // Obtain a block-contiguous view on A transposed
     // TODO
 
     // Obtain a block-contiguous view on B
     // TODO
 
-    // TODO also obtain a block-contiguous view on C
-
+    // Obtain a block-contiguous view on C
+    // TODO
 
     // For each row i of A
     for (int i = 0; i < M; ++i) {
@@ -75,6 +76,28 @@ static void do_micro_block(int lda,
     }
 }
 
+/*
+ * Assuming A and B are ldaxlda matrices, repack A into B. Repacks so that contiguous row-major blockSize elements are
+ * adjacent in memory.
+ */
+void repack_row_major(int lda, int blockSize, double* A, double* B) {
+    unsigned int idx = 0;
+    // For each block row of A
+    for (unsigned int bi = 0; bi < lda; bi += blockSize) {
+        // For each block column of A
+        for (unsigned int bk = 0; bk < lda; bk += blockSize) {
+            // For each row of the block
+            for (unsigned int i = 0; i < blockSize; ++i) {
+                // For each column of the block
+                for (unsigned int k = 0; k < min(blockSize, lda - bi); ++k) {
+                    B[idx] = A[bi + i + (bk + k) * lda];
+                    idx++;
+                }
+            }
+        }
+    }
+}
+
 /* This routine performs a dgemm operation
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format.
@@ -82,6 +105,25 @@ static void do_micro_block(int lda,
 void square_dgemm(int lda, double* A, double* B, double* C) {
     double* A_block = _mm_malloc(BI_SIZE * BI_SIZE * sizeof(double), 64);
     double* B_block = _mm_malloc(BI_SIZE * BI_SIZE * sizeof(double), 64);
+    double* C_block = _mm_malloc(BI_SIZE * BI_SIZE * sizeof(double), 64);
+
+    double* A_repacked = _mm_malloc(lda * lda * sizeof(double), 64); // TODO experiment with 32 instead
+    repack_row_major(lda, 4, A, A_repacked);
+
+    printf("A: \n");
+    // For each row i of A
+    for (unsigned int i = 0; i < lda; ++i) {
+        // For each column j of A
+        for (unsigned int j = 0; j < lda; ++j) {
+            printf("%f ", A[i + j * lda]);
+        }
+        printf("\n");
+    }
+
+    printf("A repacked: \n");
+    for (unsigned int i = 0; i < lda * lda; ++i) {
+        printf("%f \n", A_repacked[i]);
+    }
 
     // TODO implement a higher-level block size above the micro-level
     // For each block column bj of B
@@ -94,13 +136,15 @@ void square_dgemm(int lda, double* A, double* B, double* C) {
                 int M = min(BLOCK_SIZE, lda - bi);
                 int N = min(BLOCK_SIZE, lda - bj);
                 int K = min(BLOCK_SIZE, lda - bk);
-                do_micro_block(lda, M, N, K, bj, bi, bk, A + bi + bk * lda, B + bk + bj * lda, C + bi + bj * lda, A_block, B_block);
+                do_micro_block(lda, M, N, K, bj, bi, bk, A, B, C, A_block, B_block, C_block);
             }
         }
     }
 
     free(A_block);
     free(B_block);
+    free(C_block);
+    free(A_repacked);
 
     // For each block-row of A
     for (int i = 0; i < lda; i += BLOCK_SIZE) {
