81ed52ab55c083dbcdd61a36c69824df05a5e2b3
aemous <amoustafa0518@gmail.com>
2024-02-10 13:48:59 -0500

    Half smallest block size

--- hw1/dgemm-blocked.c
+++ hw1/dgemm-blocked.c
@@ -3,7 +3,7 @@
 const char* dgemm_desc = "Simple blocked dgemm.";
 
 #ifndef B1_SIZE
-#define B1_SIZE 8
+#define B1_SIZE 4
 #endif
 
 #ifndef B2_SIZE
@@ -21,11 +21,11 @@ const char* dgemm_desc = "Simple blocked dgemm.";
 static void do_block(int lda, int ldaRounded, int M, int N, int K, int bi, int bj, int bk, double* A, double* B, double* C, double* dotProduct) {
 //    double* AT = _mm_malloc(K * M * sizeof(double), 64); // K rows, M columns
     __m256d rowA1; // stores first quarter of row i of A
-    __m256d rowA2; // stores second quarter of row i of A
+//    __m256d rowA2; // stores second quarter of row i of A
 //    __m256d rowA3; // stores third quarter of row i of A
 //    __m256d rowA4; // stores fourth quarter of row i of A
     __m256d colB1; // stores first quarter of column j of B
-    __m256d colB2; // stores second quarter of column j of B
+//    __m256d colB2; // stores second quarter of column j of B
 //    __m256d colB3; // stores third quarter of column j of B
 //    __m256d colB4; // stores fourth quarter of column j of B
 
@@ -88,17 +88,17 @@ static void do_block(int lda, int ldaRounded, int M, int N, int K, int bi, int b
 //            colB3 = _mm256_load_pd(B + 8 + j * ldaRounded);
 //            colB4 = _mm256_load_pd(B + 12 + j * ldaRounded);
             rowA1 = _mm256_load_pd(A + i * ldaRounded);
-            rowA2 = _mm256_load_pd(A + 4 + i * ldaRounded);
+//            rowA2 = _mm256_load_pd(A + 4 + i * ldaRounded);
 //            rowA3 = _mm256_load_pd(A + 8 + i * ldaRounded);
 //            rowA4 = _mm256_load_pd(A + 12 + i * ldaRounded);
             colB1 = _mm256_load_pd(B + j * ldaRounded);
-            colB2 = _mm256_load_pd(B + 4 + j * ldaRounded);
+//            colB2 = _mm256_load_pd(B + 4 + j * ldaRounded);
 //            colB3 = _mm256_load_pd(B + 8 + j * ldaRounded);
 //            colB4 = _mm256_load_pd(B + 12 + j * ldaRounded);
 
             // compute first 'half' of the dot product of A[i,:] and B[:,j]
-            __m256d dot1 = _mm256_hadd_pd(_mm256_mul_pd(rowA1, colB1), _mm256_mul_pd(rowA2, colB2));
-//            __m256d dot1 = _mm256_mul_pd(rowA1, colB1);
+//            __m256d dot1 = _mm256_hadd_pd(_mm256_mul_pd(rowA1, colB1), _mm256_mul_pd(rowA2, colB2));
+            __m256d dot1 = _mm256_mul_pd(rowA1, colB1);
             // compute second 'half' of the dot product of A[i,:] and B[:,j]
 //            __m256d dot2 = _mm256_hadd_pd(_mm256_mul_pd(rowA3, colB3), _mm256_mul_pd(rowA4, colB4));
 //
