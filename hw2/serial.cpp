26091e1bfb7983c9b86d077fc6860a9f7536ff26
aemous <amoustafa0518@gmail.com>
2024-02-23 10:45:44 -0500

    Replace min, alter loop order

--- hw2/serial.cpp
+++ hw2/serial.cpp
@@ -6,17 +6,16 @@
 int num_cells = 0;
 double cellSize = 0.;
 
-//std::vector<std::vector<std::vector<particle_t*>>> cells;
 std::vector<std::vector<std::vector<int>>> cells;
 
 // TODO this may change if our cells end up not being square after parallelism
-int get_cell_x(double size, particle_t& p) {
-    return (int) ((num_cells - 1) * fmin((p.x / (size - cellSize)), 1.0));
+int get_cell_x(double size, double x) {
+    return ((num_cells - 1) * std::min((x / (size - cellSize)), 1.0));
 }
 
 // TODO this may change if our cells end up not being square after parallelism
-int get_cell_y(double size, particle_t& p) {
-    return (int) ((num_cells-1) * fmin((p.y / (size - cellSize)), 1.0));
+int get_cell_y(double size, double y) {
+    return ((num_cells-1) * std::min((y / (size - cellSize)), 1.0));
 }
 
 // Apply the force from neighbor to particle
@@ -80,9 +79,7 @@ void init_simulation(particle_t* parts, int num_parts, double size) {
 
     // map the particles to their proper cells based on their position
     for (int p = 0; p < num_parts; ++p) {
-//        cells.at(get_cell_x(size, parts[p])).at(get_cell_y(size, parts[p])).insert(&parts[p]);
-//        cells.at(get_cell_x(size, parts[p])).at(get_cell_y(size, parts[p])).push_back(&parts[p]);
-        cells.at(get_cell_x(size, parts[p])).at(get_cell_y(size, parts[p])).push_back(p);
+        cells.at(get_cell_x(size, parts[p].x)).at(get_cell_y(size, parts[p].y)).push_back(p);
     }
 }
 
@@ -92,15 +89,13 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
         // get its cell
         // for each neighbor cell
             // compute force
-
-    // TODO try range for ?
     for (int i = 0; i < num_parts; ++i) {
-        int cell_x = get_cell_x(size, parts[i]);
-        int cell_y = get_cell_y(size, parts[i]);
+        int cell_x = get_cell_x(size, parts[i].x);
+        int cell_y = get_cell_y(size, parts[i].y);
 
         // TODO experiment with loop order?
-        for (unsigned int ii = (int) fmax(0, cell_x-1); ii <= (int) fmin(num_cells-1, cell_x+1); ++ii) {
-            for (unsigned int jj = (int) fmax(0, cell_y-1); jj <= (int) fmin(num_cells-1, cell_y+1); ++jj) {
+        for (unsigned int jj = (int) fmax(0, cell_y-1); jj <= (int) fmin(num_cells-1, cell_y+1); ++jj) {
+            for (unsigned int ii = (int) fmax(0, cell_x-1); ii <= (int) fmin(num_cells-1, cell_x+1); ++ii) {
                 for (auto & neighbor_part : cells.at(ii).at(jj)) {
                     apply_force(parts[i], parts[neighbor_part]);
                 }
@@ -109,7 +104,6 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
     }
 
     // Move Particles
-    // TODO try range for ?
     for (int i = 0; i < num_parts; ++i) {
        move(parts[i], size);
     }
@@ -122,10 +116,7 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
     }
 
     // Recompute particle cells
-    // TODO try range for ?
     for (int i = 0; i < num_parts; ++i) {
-//        cells.at(get_cell_x(size, parts[i])).at(get_cell_y(size, parts[i])).insert(&parts[i]);
-//        cells.at(get_cell_x(size, parts[i])).at(get_cell_y(size, parts[i])).emplace_back(&parts[i]);
-        cells.at(get_cell_x(size, parts[i])).at(get_cell_y(size, parts[i])).push_back(i);
+        cells.at(get_cell_x(size, parts[i].x)).at(get_cell_y(size, parts[i].y)).push_back(i);
     }
 }
