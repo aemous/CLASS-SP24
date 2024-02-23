7866229e51da23e8f1c63c6044324e45719d6ee4
aemous <amoustafa0518@gmail.com>
2024-02-23 10:36:45 -0500

    3d vec

--- hw2/serial.cpp
+++ hw2/serial.cpp
@@ -2,13 +2,12 @@
 #include <cmath>
 
 #include <vector>
-//#include <unordered_set>
 
 int num_cells = 0;
 double cellSize = 0.;
 
-//std::vector<std::vector<std::unordered_set<particle_t*>>> cells;
-std::vector<std::vector<std::vector<particle_t*>>> cells;
+//std::vector<std::vector<std::vector<particle_t*>>> cells;
+std::vector<std::vector<std::vector<int>>> cells;
 
 // TODO this may change if our cells end up not being square after parallelism
 int get_cell_x(double size, particle_t& p) {
@@ -73,16 +72,17 @@ void init_simulation(particle_t* parts, int num_parts, double size) {
 
     // initialize the grid of cells
     for (unsigned int i = 0; i < num_cells; ++i) {
-        cells.emplace_back(num_cells);
+        cells.push_back(std::vector<std::vector<int>>(num_cells));
         for (unsigned int j = 0; j < num_cells; ++j) {
-            cells.at(i).emplace_back(exp_parts_per_cell + 10);
+            cells.at(i).push_back(std::vector<int>(exp_parts_per_cell + 10));
         }
     }
 
     // map the particles to their proper cells based on their position
     for (int p = 0; p < num_parts; ++p) {
 //        cells.at(get_cell_x(size, parts[p])).at(get_cell_y(size, parts[p])).insert(&parts[p]);
-        cells.at(get_cell_x(size, parts[p])).at(get_cell_y(size, parts[p])).push_back(&parts[p]);
+//        cells.at(get_cell_x(size, parts[p])).at(get_cell_y(size, parts[p])).push_back(&parts[p]);
+        cells.at(get_cell_x(size, parts[p])).at(get_cell_y(size, parts[p])).push_back(p);
     }
 }
 
@@ -92,6 +92,8 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
         // get its cell
         // for each neighbor cell
             // compute force
+
+    // TODO try range for ?
     for (int i = 0; i < num_parts; ++i) {
         int cell_x = get_cell_x(size, parts[i]);
         int cell_y = get_cell_y(size, parts[i]);
@@ -100,13 +102,14 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
         for (unsigned int ii = (int) fmax(0, cell_x-1); ii <= (int) fmin(num_cells-1, cell_x+1); ++ii) {
             for (unsigned int jj = (int) fmax(0, cell_y-1); jj <= (int) fmin(num_cells-1, cell_y+1); ++jj) {
                 for (auto & neighbor_part : cells.at(ii).at(jj)) {
-                    apply_force(parts[i], *neighbor_part);
+                    apply_force(parts[i], parts[neighbor_part]);
                 }
             }
         }
     }
 
     // Move Particles
+    // TODO try range for ?
     for (int i = 0; i < num_parts; ++i) {
        move(parts[i], size);
     }
@@ -119,8 +122,10 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
     }
 
     // Recompute particle cells
+    // TODO try range for ?
     for (int i = 0; i < num_parts; ++i) {
 //        cells.at(get_cell_x(size, parts[i])).at(get_cell_y(size, parts[i])).insert(&parts[i]);
-        cells.at(get_cell_x(size, parts[i])).at(get_cell_y(size, parts[i])).emplace_back(&parts[i]);
+//        cells.at(get_cell_x(size, parts[i])).at(get_cell_y(size, parts[i])).emplace_back(&parts[i]);
+        cells.at(get_cell_x(size, parts[i])).at(get_cell_y(size, parts[i])).push_back(i);
     }
 }
