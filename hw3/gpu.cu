28ffa26ebe91699a484e296eab142459324f6819
aemous <amoustafa0518@gmail.com>
2024-03-18 13:19:09 -0400

    .

--- hw3/gpu.cu
+++ hw3/gpu.cu
@@ -165,7 +165,7 @@ void simulate_one_step(particle_t* parts, int num_parts, double size) {
 
     // task: prefix sum particle counts
     // use thrust::exclusive_scan on the particles/bin array. the last element should be num_parts
-    thrust::exclusive_scan(bin_counts.begin(), bin_counts.end(), bin_counts);
+    thrust::exclusive_scan(thrust::host, bin_counts.begin(), bin_counts.end(), bin_counts);
 
     // task: add the particle ids to a separate array parts_sorted
     // initialize an array, 1 entry for each cell, called last_part, initialized to -1
