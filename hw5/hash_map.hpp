ca66eeee7c8fa6d63e9e632403d4e8e18857b50b
aemous <amoustafa0518@gmail.com>
2024-04-23 15:55:16 -0400

    Fix

--- hw5/hash_map.hpp
+++ hw5/hash_map.hpp
@@ -150,7 +150,8 @@ bool HashMap::request_bin_and_block(upcxx::dist_object<upcxx::global_ptr<uint64_
     auto future = upcxx::rpc(get_target(kmer.kmer),
                              [](upcxx::global_ptr<uint64_t> &g_used, uint64_t bin, upcxx::atomic_domain<uint64_t> atomic_domain) {
                                  int dst = 0;
-                                 atomic_domain.compare_exchange(g_used, g_used + bin, 0, &dst, std::memory_order_relaxed).wait();
+                                 uint64_t* used_local = g_used->local();
+                                 atomic_domain.compare_exchange(g_used, g_used[bin], (uint64_t) 0, &dst, std::memory_order_relaxed).wait();
                                  bool success = dst != 0;
                                  return success;
                              }, g_used, bin, atomic_domain);
